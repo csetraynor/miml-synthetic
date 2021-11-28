f1c97d6bc029c8d5a144f630d1a2911983d6c830
csetraynor <carlos.serratraynor@astrazeneca.com>
2021-11-28 10:36:07 +0000

    update readme

--- Scripts/02_tabnet.R
+++ Scripts/02_tabnet.R
@@ -1,70 +1,28 @@
-rm(list = ls())
-
-.libPaths(c("./ProjectLibrary/3.5",
-            "/opt/scp/software/mro/3.5.1-foss-2017a/lib64/R/site-library",
-            "/opt/scp/software/mro/3.5.1-foss-2017a/lib64/R/library") )
-
 library(readr)
 library(dplyr)
-library(drake)
-library(future.batchtools)
-future::plan(batchtools_slurm, template = "../SLURM/batchtools_gpu_slurm.tmpl")
 
-tabnet_impute_float <- function(cor_level, N, s, prop, boot, mech, M = 20) {
+tabnet_impute <- function(mech, prop, boot, type, M = 20, dropout = 0.1, gpu = T) {
   
-  foo <- file.path("DerivedData/simulations/float", cor_level, N, s, prop, boot, mech)
+  foo <- file.path("DerivedData/rwd/amputated", mech, prop, boot, type)
   mis_data <- read_csv(file.path( foo, "amputed_data.csv") )
-  
+  mis_data <- mis_data %>%
+    mutate_at(c("ALK", "EGFR", "KRAS", "BRAF", "PDL1", "event_status"), ~ as.factor( as.character(.)) )
   ## load dev mice
   devtools::document("mice")
   
-  imp <- mice(mis_data, m = M, maxit = 5, method = "tabnetRegressor", gpu = T, hpc = F )
+  imp <- mice(mis_data,
+              m = M,
+              maxit = 3,
+              method = "tabnetClassifier",
+              gpu = F,
+              hpc = F,
+              dropout = dropout,
+              embedding_cols = colnames(mis_data %>% select_if(is.factor)))
   
-  resPath <- file.path("Results/simulations/float", cor_level, N, s, prop, boot, mech, "tabnet")
+  resPath <- file.path("Results/rwd/amputated", mech, prop, boot, type, "tabnet")
   
   if(!dir.exists(resPath)) dir.create(resPath, recursive = TRUE)
   saveRDS(imp, file.path(resPath, "imp.RDS") )
   
   return(0)
 }
-
-
-run_plan <- drake_plan (
-  imp_mar = target(
-    tabnet_impute_float(cor_level, N, s, prop, boot, mech, M = 20),
-    transform = cross(cor_level = !!c("high_cor", "low_cor"),
-                    N = !!c(1e3),
-                    s = !!c(1:200),
-                    prop = !!c(0.5),
-                    boot = !!c(1),
-                    mech = !!("MAR")
-                    ),
-    hpc = TRUE,
-    resources = list(ncpus = 2, walltime = 48*60, memory = 8000)
-  ),
-  imp_mcar = target(
-    tabnet_impute_float(cor_level, N, s, prop, boot, mech, M = 20),
-    transform = cross(cor_level = !!c("high_cor"),
-                      N = !!c(1e3),
-                      s = !!c(1:200),
-                      prop = !!c(0.5),
-                      boot = !!c(1),
-                      mech = !!("MCAR")
-    ),
-    hpc = TRUE,
-    resources = list(ncpus = 2, walltime = 48*60, memory = 8000)
-  )
-)
-
-make(run_plan,
-     verbose = 3,
-     jobs = nrow(run_plan),
-     parallelism = "future",
-     lock_envir = FALSE,
-     log_progress = TRUE,
-     lazy_load = "promise",
-     console_log_file = "slurm_run.out",
-     memory_strategy = "lookahead",
-     caching = "worker",
-     keep_going = FALSE )
-
--- Scripts/Readme.txt
+++ Scripts/Readme.txt
@@ -1,1 +1,6 @@
-This directory is for R scripts to be run in the main project directory
+This directory includes R scripts to reproduce the synthetic data experiments and tested imputation methods.
+Scripts to generate the synthetic dataset:
+01_ampute.R and 01_simulate.R
+Scripts to perform the imputation:
+02_<imputation>.R
+To use MITABNET, add the mice.impute.tabnetRegressor.R in the mice package R folder: https://github.com/amices/mice and run 02_tabnet.R 
